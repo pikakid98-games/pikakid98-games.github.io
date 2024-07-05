2e70f746de3726a16d5e948c109b027ba391f0b9
Pikakid98 <12392651+pikakid98@users.noreply.github.com>
2024-07-05 19:29:06 +0100

    unreleased projects page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1547},"pages":[{"id":"page-index","displayName":"index","link":{"linkType":"LinkTypePage","href":"#!page-index"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-about","displayName":"about","link":{"linkType":"LinkTypePage","href":"#!page-about"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1547},"pages":[{"id":"page-index","displayName":"index","link":{"linkType":"LinkTypePage","href":"#!page-index"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-about","displayName":"about","link":{"linkType":"LinkTypePage","href":"#!page-about"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-unreleased-projects","displayName":"unreleased projects","link":{"linkType":"LinkTypePage","href":"#!page-unreleased-projects"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
